c011b20a54c03123e530fc2e27a56d244610b870
chenjiang <cj@itcast.cn>
2019-06-12 23:56:18 +0800

    再一次

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("又一次");
         System.out.println("zaiyi");
+        System.out.println("再一次");
     }
 }
